885f02dfc0ff69d4cb7cf2f318928737d4c880dc
Saif <213877@student.upm.edu.my>
2024-06-06 01:54:38 +0800

    message

--- android/app/src/main/java/io/flutter/plugins/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package io.flutter.plugins;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // Optional: You can add custom logic or configurations here
-}
