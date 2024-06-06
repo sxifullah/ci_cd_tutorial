529267a773a762342a1d790e8209de97c027c33c
Saif <213877@student.upm.edu.my>
2024-06-06 18:15:11 +0800

    message

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:lab5_p2/main.dart'; // Replace with the actual import for your main.dart
 
 void main() {
-  testWidgets('Sample Test', (WidgetTester tester) async {
-    // Add your test cases here
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('Navigate through pages and perform basic interactions',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      // Verify we are on the ActivationPage
+      expect(find.text('Welcome !'), findsOneWidget);
+    });
   });
 }
